1106ab7b2b26467929b69d1a0bb27cf925ea3d32
rajvindergithub <rajvindergithub@gmail.com>
2025-03-01 13:42:23 +0530

    wordpress files

---
+++ front-page.php
@@ -0,0 +1,136 @@
+<?php 
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ */
+
+get_header(); ?>
+
+
+<section id="banner_main">
+    <div class="container">
+        <div class="row">
+            <div class="b_m_heading t-upper f-800">Connecting</div>
+            <div class="b_m_s_heading t-upper f-700">airlines to</div>
+            <div class="b_m_img"><img src="<?php echo THEME_PATH; ?>/assets/images/banner_plan.png"></div>
+            <div class="b_m_img_israel">img_israel</div>
+            <div class="b_m_img_israel_heading"> Your Gateway to Success!</div>
+        </div>
+    </div>
+</section>
+<!-- banner_main -->
+
+<section id="home_about-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="h-a-us-m-h">About Us</div>
+                <div class="h-a-us-h">
+                    <h1>We are your future GSA in <span>ISRAEL</span></h1>
+                </div>
+                <div class="h-a-us-para">
+                    <p>
+                        Our goal is to help the airlines that we represent maintain an active presence among the travel agencies and tour operators in Israel.  years of experience on the tourism scene guarantees our access to the Israeli market, where our name and our success is well recognized. Our success is maintained through our dedication to the needs of our clients.
+                    </p>
+                    <p>
+                        Our goal is to help the airlines that we represent maintain an active presence among the travel agencies and tour operators in Israel.  years of experience on the tourism scene guarantees our access to the Israeli market, where our name and our success is well recognized. Our success is maintained through our dedication to the needs of our clients.
+                    </p>
+                    <p>
+                        Our goal is to help the airlines that we represent maintain an active presence among the travel agencies and tour operators in Israel.  years of experience on the tourism scene guarantees our access to the Israeli market, where our name and our success is well recognized. Our success is maintained through our dedication to the needs of our clients.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                img
+            </div>
+        </div>
+    </div>
+</section>
+<!-- home_about-us -->
+
+
+
+<section id="home_industry-overview">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="h-i-o-h-m">
+                    Industry Overview
+                </div>
+                <div class="h-i-o-h">
+                    <h2>The Travel Industry In <span>ISRAEL</span></h2>    
+                </div>
+                <div class="h-i-o-para">
+                    <p>Israel, in despite its small size, provides a huge market for Airlines, due to its significance to the three major religions of the world.</p>
+                     <p>Israel, in despite its small size, provides a huge market for Airlines, due to its significance to the three major religions of the world.</p>
+                     <p>Israel, in despite its small size, provides a huge market for Airlines, due to its significance to the three major religions of the world.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- home_industry-overview -->
+
+<section id="home_marketing_strategy">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="h-m-s-h-m">Marketing Strategy</div>
+                <div class="h-m-s-h">
+                    <h2>OUR MARKETING <span>DEPARTMENT</span></h2>
+                </div>
+                <div class="h-m-s-para">
+                    <p>Our experienced staff is fully dedicated to promote the companies we represent in a variety of areas:</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="h-m-s-block">
+                    <div class="h-m-s-b-img">img</div>
+                    <div class="h-m-s-b-heading">Direct promotion</div>
+                    <div class="h-m-s-b-line"><span>&nbsp;</span></div>
+                    <div class="h-m-s-b-para">Sales personnel visit the travel agencies daily to provide all the necessary information - new airlines routes, fares and schedules .</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- home_marketing_strategy --> 
+
+
+<section id="home_by-director">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">img</div>
+            <div class="col-md-6">
+                <div class="h-d-h-m">by Director</div>
+                <div class="h-d-h">PERSONNEL AND <span>PREMISES</span></div>
+                <div class="h-d-para">
+                    <p>Our goals have been achieved through the dedication of our highly qualified staff experienced in boosting collaboration between tour operators, travel agencies and our representative Airlines.</p>
+                    <p>Our goals have been achieved through the dedication of our highly qualified staff experienced in boosting collaboration between tour operators, travel agencies and our representative Airlines.</p>
+                    <p>Our goals have been achieved through the dedication of our highly qualified staff experienced in boosting collaboration between tour operators, travel agencies and our representative Airlines.</p>
+                </div>
+                <div class="h-d-line">&nbsp;</div>
+                <div class="h-d-sign"></div>
+                <div class="h-d-name"></div>
+                <div class="h-d-pos">Managing Director | GSA AIRLINES Israel</div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- home_by-director --> 	
+ 
+
+<section id="home_about-us">
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>
+</section>
+<!-- home_about-us --> 	
+	 
+
+<?php get_footer(); ?>
---
+++ functions.php
@@ -0,0 +1,65 @@
+<?php
+/** 
+ * For more info: https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ */	
+
+
+define('THEME_PATH', get_template_directory_uri());
+
+// bs css and js enq
+function enqueue_bootstrap() {
+    // Enqueue Bootstrap CSS
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', array(), '5.3.3', 'all');
+    
+    wp_enqueue_style('style', get_template_directory_uri().'/assets/css/style.css', array(), '5.3.3', 'all');
+
+    // Enqueue Bootstrap JavaScript
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.3', true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_bootstrap');
+
+
+
+	
+// Theme support options
+require_once(get_template_directory().'/functions/theme-support.php'); 
+
+// WP Head and other cleanup functions
+require_once(get_template_directory().'/functions/cleanup.php'); 
+
+// Register scripts and stylesheets
+require_once(get_template_directory().'/functions/enqueue-scripts.php'); 
+
+// Register custom menus and menu walkers
+require_once(get_template_directory().'/functions/menu.php'); 
+
+// Register sidebars/widget areas
+require_once(get_template_directory().'/functions/sidebar.php'); 
+
+// Makes WordPress comments suck less
+require_once(get_template_directory().'/functions/comments.php'); 
+
+// Replace 'older/newer' post links with numbered navigation
+require_once(get_template_directory().'/functions/page-navi.php'); 
+
+// Adds support for multiple languages
+require_once(get_template_directory().'/functions/translation/translation.php'); 
+
+// Adds site styles to the WordPress editor
+// require_once(get_template_directory().'/functions/editor-styles.php'); 
+
+// Remove Emoji Support
+// require_once(get_template_directory().'/functions/disable-emoji.php'); 
+
+// Related post function - no need to rely on plugins
+// require_once(get_template_directory().'/functions/related-posts.php'); 
+
+// Use this as a template for custom post types
+// require_once(get_template_directory().'/functions/custom-post-type.php');
+
+// Customize the WordPress login menu
+// require_once(get_template_directory().'/functions/login.php'); 
+
+// Customize the WordPress admin
+// require_once(get_template_directory().'/functions/admin.php'); 
